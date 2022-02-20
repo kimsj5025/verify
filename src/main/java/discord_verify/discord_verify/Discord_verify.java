37abcbb2a69ffb3ae7a5bea767bff29b5a1137ca
kimsj5025 <kimsj5025@naver.com>
2022-02-20 13:18:39 +0900

    기초커밋

---
+++ src/main/java/discord_verify/discord_verify/Discord_verify.java
@@ -0,0 +1,17 @@
+package discord_verify.discord_verify;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Discord_verify extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
